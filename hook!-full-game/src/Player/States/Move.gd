10af5bc6cdb7cb852873a6bc4e28121130124959
Brad <DrNeroCF@gmail.com>
2019-07-24 15:28:36 -0500

    First level pass and feedback
    I've fiddled a tiny bit with the code, mostly to make the camera smoother, plus the first pass on the level, though I have feedback on the issue I created, but I think letting the player boost more from hook points would allow for more varied placement of the hook points, but I'd like to discuss that change further.

--- hook!-full-game/src/Player/States/Move.gd
+++ hook!-full-game/src/Player/States/Move.gd
@@ -45,6 +45,7 @@ func unhandled_input(event: InputEvent) -> void:
 
 func physics_process(delta: float) -> void:
 	self.velocity = calculate_velocity(velocity, speed, acceleration, delta, get_move_direction())
+	
 	self.velocity = owner.move_and_slide(velocity, owner.FLOOR_NORMAL)
 	Events.emit_signal("player_moved", owner)
 
@@ -61,15 +62,15 @@ static func calculate_velocity(
 		old_velocity: Vector2, max_speed: Vector2, acceleration: Vector2,
 		delta: float, move_direction: Vector2) -> Vector2:
 	var new_velocity: = old_velocity
-
+	
 	new_velocity += move_direction * acceleration * delta
 	new_velocity.x = clamp(new_velocity.x, -max_speed.x, max_speed.x)
 	new_velocity.y = clamp(new_velocity.y, -max_speed.y, max_speed.y)
-
+	
 	return new_velocity
 
 
 static func get_move_direction() -> Vector2:
 	return Vector2(
 			Input.get_action_strength("move_right") - Input.get_action_strength("move_left"),
-			1.0)
+			0.8)
